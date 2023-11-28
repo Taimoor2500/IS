66996e2847a6148f68f8279996563709a96e5443
Tamoor2500 <taimoorrashid858@gmail.com>
2023-11-29 01:00:53 +0500

    added code

--- stfinal.py
+++ stfinal.py
@@ -1,3 +1,5 @@
+import streamlit as st
+from stegano import stegano  # Replace with your actual stegano module
 import numpy as np
 
 def caesar_cipher(text, key):
@@ -11,7 +13,6 @@ def caesar_cipher(text, key):
     return result
 
 def encrypt_cover_text(file_name, key):
-    
     with open(file_name, "r") as file:
         plaintext = file.read()
 
@@ -20,9 +21,7 @@ def encrypt_cover_text(file_name, key):
     with open("covertext_encrypted.txt", "w") as file:
         file.write(encrypted_text)
 
-    print("Cover text encrypted and saved in 'covertext_encrypted.txt'.")
-
-    txt_encode("VendorSphere"+ str(key), "covertext_encrypted.txt")
+    stegano.txt_encode("VendorSphere" + str(key), "covertext_encrypted.txt")
 
 def caesar_decipher(text, key):
     return caesar_cipher(text, -key)
@@ -31,19 +30,16 @@ def remove_unicode(file_path, output_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
 
-    # Filter out non-ASCII characters
     cleaned_text = ''.join(char for char in text if ord(char) < 128)
 
     with open(output_path, 'w', encoding='utf-8') as output_file:
         output_file.write(cleaned_text)
 
 def decrypt_cover_text(inputFile, key):
-    
     input_file_path = inputFile
     output_file_path = inputFile
     remove_unicode(input_file_path, output_file_path)
     with open(output_file_path, "r") as file:
-
         encrypted_text = file.read()
     
     decrypted_text = caesar_decipher(encrypted_text, key)
@@ -51,78 +47,54 @@ def decrypt_cover_text(inputFile, key):
     with open("covertext_decrypted.txt", "w") as file:
         file.write(decrypted_text)
 
-    print("Cover text decrypted and saved in 'covertext_decrypted.txt'.")
-
 def txt_encode(text, filee):
-    l=len(text)
-    i=0
-    add=''
-    while i<l:
-        t=ord(text[i])
-        if(t>=32 and t<=64):
-            t1=t+48
-            t2=t1^170       #170: 10101010
+    l = len(text)
+    i = 0
+    add = ''
+    while i < l:
+        t = ord(text[i])
+        if(t >= 32 and t <= 64):
+            t1 = t + 48
+            t2 = t1 ^ 170
             res = bin(t2)[2:].zfill(8)
-            add+="0011"+res
-
+            add += "0011" + res
         else:
-            t1=t-48
-            t2=t1^170
+            t1 = t - 48
+            t2 = t1 ^ 170
             res = bin(t2)[2:].zfill(8)
-            add+="0110"+res
-        i+=1
-    res1=add+"111111111111"
-    print("The string after binary conversion appyling all the transformation :- " + (res1))
-    length = len(res1)
-    print("Length of binary after conversion:- ",length)
-    HM_SK=""
-    ZWC={"00":u'\u200C',"01":u'\u202C',"11":u'\u202D',"10":u'\u200E'}
-    file1 = open(filee,"r+")
-    nameoffile = input("\nEnter the name of the Stego file after Encoding(with extension):- ")
-    file3= open(nameoffile,"w+", encoding="utf-8")
-    word=[]
+            add += "0110" + res
+        i += 1
+    res1 = add + "111111111111"
+    HM_SK = ""
+    ZWC = {"00": u'\u200C', "01": u'\u202C', "11": u'\u202D', "10": u'\u200E'}
+    file1 = open(filee, "r+")
+    nameoffile = input("\nEnter the name of the Stego file after Encoding (with extension): ")
+    file3 = open(nameoffile, "w+", encoding="utf-8")
+    word = []
     for line in file1:
-        word+=line.split()
-    i=0
-    while(i<len(res1)):
-        s=word[int(i/12)]
-        j=0
-        x=""
-        HM_SK=""
-        while(j<12):
-            x=res1[j+i]+res1[i+j+1]
-            HM_SK+=ZWC[x]
-            j+=2
-        s1=s+HM_SK
+        word += line.split()
+    i = 0
+    while(i < len(res1)):
+        s = word[int(i/12)]
+        j = 0
+        x = ""
+        HM_SK = ""
+        while(j < 12):
+            x = res1[j+i] + res1[i+j+1]
+            HM_SK += ZWC[x]
+            j += 2
+        s1 = s + HM_SK
         file3.write(s1)
         file3.write(" ")
-        i+=12
-    t=int(len(res1)/12)
-    while t<len(word):
+        i += 12
+    t = int(len(res1)/12)
+    while t < len(word):
         file3.write(word[t])
         file3.write(" ")
-        t+=1
+        t += 1
     file3.close()
     file1.close()
-    print("\nStego file has successfully generated")
-
-# def encode_txt_data(filename_):
-#     count2=0
-#     file1 = open(filename_,"r")
-#     for line in file1:
-#         for word in line.split():
-#             count2=count2+1
-#     file1.close()
-#     bt=int(count2)
-#     print("Maximum number of words that can be inserted :- ",int(bt/6))
-#     text1=input("\nEnter data to be encoded:- ")
-#     l=len(text1)
-#     if(l<=bt):
-#         print("\nInputed message can be hidden in the cover file\n")
-#         txt_encode(text1)
-#     else:
-#         print("\nString is too big please reduce string size")
-#         encode_txt_data()
+    print("\nStego file has been successfully generated")
 
 def BinaryToDecimal(binary):
     string = int(binary, 2)
@@ -178,68 +150,44 @@ def decode_txt_data():
 
     print("\nMessage after decoding from the stego file:", final)
 
-    # Mark the section of the file that contains the encoded message
     marker_start = "#START_ENCODED_MESSAGE#"
     marker_end = "#END_ENCODED_MESSAGE#"
 
-    # Remove the marked section from the file
     with open(stego, "w", encoding="utf-8") as file4:
         for line in lines:
             if not is_hidden_message or (marker_start not in line and marker_end not in line):
                 file4.write(line)
 
-
-# def txt_steg():
-#     while True:
-#         print("\n\t\tTEXT STEGANOGRAPHY OPERATIONS")
-#         print("1. Encode the Text message")
-#         print("2. Decode the Text message")
-#         print("3. Exit")
-#         choice1 = int(input("Enter the Choice:"))
-#         if choice1 == 1:
-#             encode_txt_data()
-#         elif choice1 == 2:
-#             decrypted=decode_txt_data()
-#         elif choice1 == 3:
-#             break
-#         else:
-#             print("Incorrect Choice")
-#         print("\n")
-
 def msgtobinary(msg):
     if type(msg) == str:
-        result= ''.join([ format(ord(i), "08b") for i in msg ])
-
+        result = ''.join([format(ord(i), "08b") for i in msg])
     elif type(msg) == bytes or type(msg) == np.ndarray:
-        result= [ format(i, "08b") for i in msg ]
-
+        result = [format(i, "08b") for i in msg]
     elif type(msg) == int or type(msg) == np.uint8:
-        result=format(msg, "08b")
-
+        result = format(msg, "08b")
     else:
         raise TypeError("Input type is not supported in this function")
-
     return result
 
 def KSA(key):
     key_length = len(key)
-    S=list(range(256))
-    j=0
+    S = list(range(256))
+    j = 0
     for i in range(256):
-        j=(j+S[i]+key[i % key_length]) % 256
-        S[i],S[j]=S[j],S[i]
+        j = (j + S[i] + key[i % key_length]) % 256
+        S[i], S[j] = S[j], S[i]
     return S
 
-def PRGA(S,n):
-    i=0
-    j=0
-    key=[]
-    while n>0:
-        n=n-1
-        i=(i+1)%256
-        j=(j+S[i])%256
-        S[i],S[j]=S[j],S[i]
-        K=S[(S[i]+S[j])%256]
+def PRGA(S, n):
+    i = 0
+    j = 0
+    key = []
+    while n > 0:
+        n = n-1
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+        K = S[(S[i] + S[j]) % 256]
         key.append(K)
     return key
 
@@ -248,60 +196,47 @@ def preparing_key_array(s):
 
 def encryption(plaintext):
     print("Enter the key : ")
-    key=input()
-    key=preparing_key_array(key)
+    key = input()
+    key = preparing_key_array(key)
 
-    S=KSA(key)
+    S = KSA(key)
 
-    keystream=np.array(PRGA(S,len(plaintext)))
-    plaintext=np.array([ord(i) for i in plaintext])
+    keystream = np.array(PRGA(S, len(plaintext)))
+    plaintext = np.array([ord(i) for i in plaintext])
 
-    cipher=keystream^plaintext
-    ctext=''
+    cipher = keystream ^ plaintext
+    ctext = ''
     for c in cipher:
-        ctext=ctext+chr(c)
+        ctext = ctext + chr(c)
     return ctext
 
 def decryption(ciphertext):
     print("Enter the key : ")
-    key=input()
-    key=preparing_key_array(key)
+    key = input()
+    key = preparing_key_array(key)
 
-    S=KSA(key)
+    S = KSA(key)
 
-    keystream=np.array(PRGA(S,len(ciphertext)))
-    ciphertext=np.array([ord(i) for i in ciphertext])
+    keystream = np.array(PRGA(S, len(ciphertext)))
+    ciphertext = np.array([ord(i) for i in ciphertext])
 
-    decoded=keystream^ciphertext
-    dtext=''
+    decoded = keystream ^ ciphertext
+    dtext = ''
     for c in decoded:
-        dtext=dtext+chr(c)
+        dtext = dtext + chr(c)
     return dtext
 
-def main():
-    print("\t\t      STEGANOGRAPHY")
-    while True:
-        print("\n\t\t\tMAIN MENU\n")
-        print("1. Encrypt and Encode file")
-        print("2. Decrypt file")
-        print("3. DecodeStego file")
-        print("4. Exit\n")
-        choice1 = int(input("Enter the Choice: "))
-        if choice1 == 1:
-            fileName = str(input("Enter the File Name to encrypt and encode: "))
-            key = int(input("Enter the key: "))
-            encrypt_cover_text(fileName, key)
-        elif choice1 == 2:
-            file_name = str(input("Enter the File Name to decrypt and decode: "))
-            key = int(input("Enter the key: "))
-            decrypt_cover_text(file_name, key)
-        elif choice1 == 3:
-            decode_txt_data()
-        elif choice1 == 4:
-           break
-        else:
-            print("Incorrect Choice")
-        print("\n\n")
+option = st.radio("Select an option:", ("Encrypt and Encode", "Decrypt and Decode"))
 
-if __name__ == "__main__":
-    main()
+if option == "Encrypt and Encode":
+    key = st.number_input("Enter the key:", min_value=0, step=1)
+    plaintext = st.text_area("Enter the text to encrypt:")
+    if st.button("Encrypt and Encode"):
+        encrypted_text = caesar_cipher(plaintext, key)
+        steg_text = stegano.txt_encode("VendorSphere" + str(key), encrypted_text)
+        st.success("Encryption and encoding completed successfully.")
+        st.text("Encrypted and encoded text:")
+        st.text(steg_text)
+
+elif option == "Decrypt and Decode":
+    st.warning("This feature is not implemented in this example.")
